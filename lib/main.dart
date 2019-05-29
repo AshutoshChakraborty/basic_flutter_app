9242b0a816352cb8793420fc7bafcc0d38ab8796
Ashutosh Chakraborty <ashutosh.c@encoders.co.in>
2019-05-29 15:02:19 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:basic_flutter_app/pages/page_one.dart';
+import 'package:flutter/material.dart';
+
+void main()=> runApp(PageOne());
+
+
---
+++ lib/pages/page_one.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Page One"),
+        ),
+      ),
+    );
+  }
+
+}
+
